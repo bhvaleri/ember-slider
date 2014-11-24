7c577da56c92d956a0f5d868cc257bb7305f5987
Bill <billvaleriote@gmail.com>
2014-11-23 16:16:22 -0800

    Simple circle example in app

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,5 +3,9 @@ import Ember from "ember";
 export default Ember.Controller.extend({
 	value: 15,
 	minValue: 0,
-	maxValue: 100
+	maxValue: 100,
+
+	circleStyle: function () {
+		return "width:" + this.get('value') + "px; height: " + this.get('value') + "px;";
+	}.property('value')
 });
